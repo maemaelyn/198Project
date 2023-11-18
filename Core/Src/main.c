51315d329d2eda6efa51a19b349819d6fc9ec755
maemaelyn <149886351+maemaelyn@users.noreply.github.com>
2023-11-18 14:44:40 -0500

    Version 2: Modifying the code to take ADC Analog Signal and Convert it into BPM
    The code was unable to convert the analog signal from the pulse sensor therefore, new code and functions needed to be implemented.
    November 18, 2023

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,162 +1,152 @@
 #include "main.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include "stdlib.h"
 
-#define I2C_ADDR 0x27 // I2C address of the PCF8574
-#define RS_BIT 0 // Register select bit
-#define EN_BIT 2 // Enable bit
-#define BL_BIT 3 // Backlight bit
-#define D4_BIT 4 // Data 4 bit
-#define D5_BIT 5 // Data 5 bit
-#define D6_BIT 6 // Data 6 bit
-#define D7_BIT 7 // Data 7 bit
-#define LCD_ROWS 2 // Number of rows on the LCD
-#define LCD_COLS 16 // Number of columns on the LCD
+ADC_HandleTypeDef hadc1;
+
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_ADC1_Init(void);
-uint8_t backlight_state = 1; //is this necessary?
-/* Private variables */
-ADC_HandleTypeDef hadc1;
-I2C_HandleTypeDef hi2c1;
-UART_HandleTypeDef huart2;
-void lcd_write_nibble(uint8_t nibble, uint8_t rs);
-void lcd_send_cmd(uint8_t cmd);
-void lcd_send_data(uint8_t data);
-void lcd_init();
-void lcd_write_string(char *str);
-void lcd_set_cursor(uint8_t row, uint8_t column);
-void lcd_clear(void);
-void lcd_backlight(uint8_t state);
-uint16_t ReadPulseSensor(void) {
-   ADC_ChannelConfTypeDef sConfig = {0}; //is this necessary?
-   ADC_HandleTypeDef hadc1;
-   HAL_ADC_Start(&hadc1);
-   HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-   return HAL_ADC_GetValue(&hadc1);
+
+extern I2C_HandleTypeDef hi2c1;  // change your handler here accordingly
+
+// Define global variable for backlight state
+uint8_t backlight_state = 1;
+
+void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
+  uint8_t data = nibble << D4_BIT;
+  data |= rs << RS_BIT;
+  data |= backlight_state << BL_BIT; // Include backlight state in data
+  data |= 1 << EN_BIT;
+  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
+  HAL_Delay(1);
+  data &= ~(1 << EN_BIT);
+  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
 }
-//main function
+
+void lcd_send_cmd(uint8_t cmd) {
+  uint8_t upper_nibble = cmd >> 4;
+  uint8_t lower_nibble = cmd & 0x0F;
+  lcd_write_nibble(upper_nibble, 0);
+  lcd_write_nibble(lower_nibble, 0);
+  if (cmd == 0x01 || cmd == 0x02) {
+    HAL_Delay(2);
+  }
+}
+
+void lcd_send_data(uint8_t data) {
+  uint8_t upper_nibble = data >> 4;
+  uint8_t lower_nibble = data & 0x0F;
+  lcd_write_nibble(upper_nibble, 1);
+  lcd_write_nibble(lower_nibble, 1);
+}
+
+void lcd_init() {
+  HAL_Delay(50);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(5);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x02, 0);
+  lcd_send_cmd(0x28);
+  lcd_send_cmd(0x0C);
+  lcd_send_cmd(0x06);
+  lcd_send_cmd(0x01);
+  HAL_Delay(2);
+}
+
+void lcd_write_string(char *str) {
+  while (*str) {
+    lcd_send_data(*str++);
+  }
+}
+
+void lcd_set_cursor(uint8_t row, uint8_t column) {
+    uint8_t address;
+    switch (row) {
+        case 0:
+            address = 0x00;
+            break;
+        case 1:
+            address = 0x40;
+            break;
+        default:
+            address = 0x00;
+    }
+    address += column;
+    lcd_send_cmd(0x80 | address);
+}
+
+void lcd_clear(void) {
+	lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+void lcd_backlight(uint8_t state) {
+  if (state) {
+    backlight_state = 1;
+  } else {
+    backlight_state = 0;
+  }
+}
+int map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
+{
+  return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+}
 int main(void)
 {
   HAL_Init();
   SystemClock_Config();
-
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_I2C1_Init();
   MX_ADC1_Init();
-
+  MX_I2C1_Init();
   lcd_init();
   lcd_backlight(1);
-  uint16_t pulseData;
-  uint16_t ReadPulseSensor(void) {
-      // Implement your pulse sensor reading code here
-      // For example, read analog data using ADC
-      ADC_HandleTypeDef hadc1;
-      HAL_ADC_Start(&hadc1);
-      HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-      return HAL_ADC_GetValue(&hadc1);
-  }
+  lcd_clear();
 
+  int adc_value, pulse_rate;
+  char pulse_rate_str[20];
+
+  /* Infinite loop */
   while (1)
   {
-	 	 pulseData= ReadPulseSensor();
-	 	 char pulseStr[5];
-	 	  sprintf(pulseStr, "%d", pulseData);
-	 	  lcd_clear();
-	 	  lcd_set_cursor(0, 0);
-	 	  lcd_write_string("BPM: ");
-	 	  lcd_set_cursor(1, 0);
-	 	  //HAL_GPIO_TogglePin(GPIOA, LD2_Pin);
-	 	  lcd_write_string(pulseStr);
-	 	  memset(pulseStr, 0, sizeof(pulseStr));
-	 	  HAL_Delay(1500);
+    HAL_ADC_Start(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1, 100);
+    adc_value = HAL_ADC_GetValue(&hadc1);
+    HAL_ADC_Stop(&hadc1);
+
+    // Assuming that the pulse sensor provides an analog signal,
+    // convert the analog value to a pulse rate.
+    // Adjust this calculation based on the specifications of your pulse sensor.
+    pulse_rate = map(adc_value, 0, 4095, 30, 220); // Adjust the range accordingly
+
+    sprintf(pulse_rate_str, "%d\n", pulse_rate);
+
+    lcd_set_cursor(0, 0);
+    lcd_write_string("Heart Rate: ");
+    lcd_set_cursor(1, 0);
+    lcd_write_string(pulse_rate_str);
+    lcd_set_cursor(1, 2);
+    lcd_send_data('bpm'); // Display beats per minute
   }
-  /* USER CODE END 3 */
 }
+
+// Function to map a value from one range to another
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-
-
-void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
-uint8_t data = nibble << D4_BIT;
-data |= rs << RS_BIT;
-data |= backlight_state << BL_BIT; // Include backlight state in data
-data |= 1 << EN_BIT;
-HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
-HAL_Delay(1);
-data &= ~(1 << EN_BIT);
-HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
-}
-void lcd_send_cmd(uint8_t cmd) {
-uint8_t upper_nibble = cmd >> 4;
-uint8_t lower_nibble = cmd & 0x0F;
-lcd_write_nibble(upper_nibble, 0);
-lcd_write_nibble(lower_nibble, 0);
-if (cmd == 0x01 || cmd == 0x02) {
-  HAL_Delay(2);
-}
-}
-void lcd_send_data(uint8_t data) {
-uint8_t upper_nibble = data >> 4;
-uint8_t lower_nibble = data & 0x0F;
-lcd_write_nibble(upper_nibble, 1);
-lcd_write_nibble(lower_nibble, 1);
-}
-void lcd_init() {
-HAL_Delay(50);
-lcd_write_nibble(0x03, 0);
-HAL_Delay(5);
-lcd_write_nibble(0x03, 0);
-HAL_Delay(1);
-lcd_write_nibble(0x03, 0);
-HAL_Delay(1);
-lcd_write_nibble(0x02, 0);
-lcd_send_cmd(0x28);
-lcd_send_cmd(0x0C);
-lcd_send_cmd(0x06);
-lcd_send_cmd(0x01);
-HAL_Delay(2);
-}
-void lcd_write_string(char *str) {
-while (*str) {
-  lcd_send_data(*str++);
-}
-}
-void lcd_set_cursor(uint8_t row, uint8_t column) {
-  uint8_t address;
-  switch (row) {
-      case 0:
-          address = 0x00;
-          break;
-      case 1:
-          address = 0x40;
-          break;
-      default:
-          address = 0x00;
-  }
-  address += column;
-  lcd_send_cmd(0x80 | address);
-}
-void lcd_clear(void) {
-	lcd_send_cmd(0x01);
-  HAL_Delay(2);
-}
-void lcd_backlight(uint8_t state) {
-if (state) {
-  backlight_state = 1;
-} else {
-  backlight_state = 0;
-}
-}
-
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -245,8 +235,17 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_I2C1_Init(void)
 {
 
